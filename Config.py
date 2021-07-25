bc0d54eeb7df2ee091e01607fe5291acd27ab229
(◍•ᴗ•◍) <79841865+loverboyXD@users.noreply.github.com>
2021-07-25 23:03:13 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,12 @@
+import os 
+
+class tele(object):
+   API_ID = int(os.environ.get("API_ID", 6)) 
+   API_HASH = os.environ.get("API_HASH", "") 
+   TOKEN = os.environ.get("TOKEN" "")
+
+# CONFIG 
+# © @Alone_loverboy 
+# ALL COPYRIGHT RESERVED ©
+# KANGER = MADARC"D, RANDI CH*D, BINA LODE WALA, 
+# KANGER = FUCKER, MOTHER FUCKER
