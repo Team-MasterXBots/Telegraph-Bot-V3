5829ec1db4c782575401e1a13199f92f2799c79d
(◍•ᴗ•◍) <79841865+loverboyXD@users.noreply.github.com>
2021-07-25 23:23:08 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,143 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telegraph import upload_file
+from Config import Config
+psycho = Client(
+    "Telegraph Uploader Bot",
+    bot_token = Config.TOKEN,
+    api_id = Config.API_ID,
+    api_hash = Config.API_HASH
+)
+DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
+
+START_TEXT = """
+H!✨
+I am Telegraph Media Converter🧳 Create by Psychobots 
+I can create Pictures under 5MB
+
+~ @Psycho_Bots ~
+"""
+HELP_TEXT = """
+- Just give me a media under 5MB
+- Then I will download it
+- I will then upload it to the telegra.ph link
+
+Support ~ @Psycho_Bots ~
+"""
+ABOUT_TEXT = """
+- **Bot :** `Telegraph Uploader`
+- **Python3 :** `3.9.6`
+- **Updates Channel: **[Psycho_Bots](t.me/Psycho_Bots)
+- **Support :** [PsychoBots](t.me/PsychoBots_chat)
+
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Support', url="t.me/PsychoBots_chat"),
+        InlineKeyboardButton('Updates', url='https://t.me/Psycho_Bots')
+        ],
+        [
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+
+@psycho.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    else:
+        await update.message.delete()
+    
+
+@psycho.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = START_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=reply_markup
+    )
+
+@psycho.on_message(filters.private & filters.media)
+async def getmedia(bot, update):
+    medianame = DOWNLOAD_LOCATION + str(update.from_user.id)
+    try:
+        message = await update.reply_message(
+            text="`Processing...`",
+            quote=True,
+            disable_web_page_preview=True
+        )
+        await bot.download_media(
+            message=update,
+            file_name=medianame
+        )
+        response = upload_file(medianame)
+        try:
+            os.remove(medianame)
+        except:
+            pass
+    except Exception as error:
+        print(error)
+        text=f"Error :- <code>{error}</code>"
+        reply_markup=InlineKeyboardMarkup(
+            [[
+            InlineKeyboardButton('More Help', callback_data='help')
+            ]]
+        )
+        await message.edit_text(
+            text=text,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
+        )
+        return
+    text=f"**Link :-** `https://telegra.ph{response[0]}`\n\n**Join :-** @psycho_Bots"
+    reply_markup=InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"),
+        InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+        ],[
+        InlineKeyboardButton(text="Join Updates Channel", url="https://telegram.me/psycho_Bots")
+        ]]
+    )
+    await message.edit_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+psycho.run()
