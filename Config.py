2798a81edf5664b21803ecca394fc8f787eb6893
(◍•ᴗ•◍) <79841865+loverboyXD@users.noreply.github.com>
2021-07-26 08:28:06 +0530

    </> @Psycho_Bots </>

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
 import os 
 
-class tele(object):
+class Config(object):
    API_ID = int(os.environ.get("API_ID", 6)) 
    API_HASH = os.environ.get("API_HASH", "") 
    TOKEN = os.environ.get("TOKEN" "")
